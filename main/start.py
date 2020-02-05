a2d805d7bc6e5b8eb69a02b5c6b26187e35dc14d
Samuelson <s4mu@protonmail.com>
2020-02-05 07:41:53 +0000

    fixed loop

--- main/start.py
+++ main/start.py
@@ -2,18 +2,22 @@ from machine import Pin
 from time import sleep
 import machine
 import sys
-import utime
 
 
 # Pin definitions
 repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 led = Pin(2, Pin.OUT)
 
+while True:
+        # If button 0 is pressed, drop to REPL
+    if repl_button.value() == 0:
+        print("Dropping to REPL")
+        sys.exit()
 
-    # If button 0 is pressed, drop to REPL
-if repl_button.value() == 0:
-    print("Dropping to REPL")
-    sys.exit()
+    led.value(not led.value())
+    sleep(0.5)
 
-led.value(not led.value())
-sleep(0.5)
+    if '.version' in os.listdir('main'):
+        import machine
+        machine.reset()
+
