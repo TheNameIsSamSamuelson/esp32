8585a9ebbf9834fce00120aabe09ba3edb416604
Samuelson <s4mu@protonmail.com>
2020-02-05 11:32:44 +0000

    return true in case of new version found

--- main/start.py
+++ main/start.py
@@ -1,19 +1,19 @@
-# from machine import Pin
+from machine import Pin
 # from time import sleep
 # import machine
 # import sys
 #
 # # Pin definitions
 # repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
-# led = Pin(2, Pin.OUT)
+led = Pin(2, Pin.OUT)
 # ctt=0
 # while ctt<3:
 #     # If button 0 is pressed, drop to REPL
 #     if repl_button.value() == 0:
 #         print("Dropping to REPL")
 #         sys.exit()
-# led.value(not led.value())
+led.value(not led.value())
 #     sleep(1)
 #     ctt+=1
 # sys.exit()
-#magaaaahiperrrr latest
+#agora vai
