b8ac73eaa7a31bfbbe49276943d01f3b58704b14
Samuelson <s4mu@protonmail.com>
2020-02-05 09:55:55 +0000

    removed problematic

--- main/start.py
+++ main/start.py
@@ -7,14 +7,13 @@ import sys
 # Pin definitions
 repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 led = Pin(2, Pin.OUT)
+
 ctt=0
-while ctt<55:
-        # If button 0 is pressed, drop to REPL
+while ctt<10:
+    # If button 0 is pressed, drop to REPL
     if repl_button.value() == 0:
         print("Dropping to REPL")
         sys.exit()
     led.value(not led.value())
-    sleep(0.5)
+    sleep(0.2)
     ctt+=1
-
-
