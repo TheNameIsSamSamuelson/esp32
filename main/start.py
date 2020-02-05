9e30310f9a45b200cfce6806db48e824c0765443
Samuelson <s4mu@protonmail.com>
2020-02-05 10:34:41 +0000

    testadsad

--- main/start.py
+++ main/start.py
@@ -7,12 +7,12 @@ import sys
 repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 led = Pin(2, Pin.OUT)
 ctt=0
-while ctt<10:
+while ctt<3:
     # If button 0 is pressed, drop to REPL
     if repl_button.value() == 0:
         print("Dropping to REPL")
         sys.exit()
     led.value(not led.value())
-    sleep(0.2)
+    sleep(1)
     ctt+=1
 sys.exit()
