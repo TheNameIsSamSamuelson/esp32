21579b299fb368bc8362b7396785b1948c21b3f4
Samuelson <s4mu@protonmail.com>
2020-02-05 11:16:16 +0000

    return true in case of new version found

--- main/ota_updater.py
+++ main/ota_updater.py
@@ -40,6 +40,9 @@ class OTAUpdater:
             with open(self.modulepath('next/.version_on_reboot'), 'w') as versionfile:
                 versionfile.write(latest_version)
                 versionfile.close()
+            return True
+        else:
+            return False
 
     def download_and_install_update_if_available(self, ssid, password):
         if 'next' in os.listdir(self.module):
