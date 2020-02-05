731234d163dc675686351af23d284a172fceddd9
Samuelson <s4mu@protonmail.com>
2020-02-05 07:28:41 +0000

    fixed

--- main/start.py
+++ main/start.py
@@ -1,8 +1,19 @@
 from machine import Pin
 from time import sleep
+import machine
+import sys
+import utime
 
+
+# Pin definitions
+repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 led = Pin(2, Pin.OUT)
 
-while True:
-  led.value(not led.value())
-  sleep(0.5)
+
+    # If button 0 is pressed, drop to REPL
+if repl_button.value() == 0:
+    print("Dropping to REPL")
+    sys.exit()
+
+led.value(not led.value())
+sleep(0.5)
